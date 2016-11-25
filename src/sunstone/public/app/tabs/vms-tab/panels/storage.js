5eba0f22e5d8701b7afbf2531fb1edc15de5f990
juanmont <jjmontiel@opennebula.org>
2016-11-25 16:55:34 +0100

    F #4393 remove the tags

--- src/sunstone/public/app/tabs/vms-tab/panels/storage.js
+++ src/sunstone/public/app/tabs/vms-tab/panels/storage.js
@@ -289,7 +289,7 @@ define(function(require) {
             if (disk.IMAGE_ID &&
                  StateActions.enabledStateAction("VM.disk_saveas", that.element.STATE, that.element.LCM_STATE)) {
               actions += '<a href="VM.disk_saveas" class="disk_saveas nowrap" >\
-              <i class="fa fa-save fa-fw"></i>' + Locale.tr("Save as") + '</a> &emsp;';
+              <i class="fa fa-save fa-fw"></i></a> &emsp;';//+ Locale.tr("Save as") + ';'
             }
           }
 
@@ -297,16 +297,16 @@ define(function(require) {
           if (Config.isTabActionEnabled("vms-tab", "VM.detachdisk")) {
             if (StateActions.enabledStateAction("VM.detachdisk", that.element.STATE, that.element.LCM_STATE) && !disk.CONTEXT) {
               actions += ('<a href="VM.detachdisk" class="detachdisk nowrap" >\
-              <i class="fa fa-times fa-fw"></i>' + Locale.tr("Detach") +
-              '</a> &emsp;');
+              <i class="fa fa-times fa-fw"></i></a> &emsp;');// + Locale.tr("Detach") +
+              
             }
           }
 
           if (Config.isTabActionEnabled("vms-tab", "VM.disk_snapshot_create")) {
             if (StateActions.enabledStateAction("VM.disk_snapshot_create", that.element.STATE, that.element.LCM_STATE) && disk.IMAGE_ID) {
               actions += ('<a href="VM.disk_snapshot_create" class="disk_snapshot_create nowrap" >\
-              <i class="fa fa-camera fa-fw"></i>' + Locale.tr("Snapshot") +
-              '</a>');
+              <i class="fa fa-camera fa-fw"></i></a>');//+ Locale.tr("Snapshot") +
+              
             }
           }
         }
